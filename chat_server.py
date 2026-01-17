540991368ebea6953e87f31dae7bf87ab0d1cc35
liranv2001-ops <liranv2001@gmail.com>
2026-01-17 13:25:45 +0200

    Add files via upload

---
+++ chat_server.py
@@ -0,0 +1,56 @@
+import socket
+import threading
+
+# הגדרות שרת - מאזין לכל הכתובות המקומיות
+HOST = '127.0.0.1'
+PORT = 5555
+
+clients = []
+nicknames = []
+
+def broadcast(message):
+    for client in clients:
+        try:
+            client.send(message)
+        except:
+            pass
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            if client in clients:
+                index = clients.index(client)
+                clients.remove(client)
+                client.close()
+                nickname = nicknames[index]
+                broadcast(f'{nickname} left the chat!'.encode('utf-8'))
+                nicknames.remove(nickname)
+                break
+
+def receive():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((HOST, PORT))
+    server.listen()
+    print(f"Server is listening on port {PORT}...")
+
+    while True:
+        client, address = server.accept()
+        print(f"Connected with {str(address)}")
+
+        client.send('NICK'.encode('utf-8'))
+        nickname = client.recv(1024).decode('utf-8')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f"Nickname of the client is {nickname}")
+        broadcast(f'{nickname} joined the chat!'.encode('utf-8'))
+        client.send('Connected to server!'.encode('utf-8'))
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+if __name__ == "__main__":
+    receive()
